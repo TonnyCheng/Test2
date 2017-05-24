9a8a8eee513c836758aaa768fc11a373b19e9ea9
TonnyCheng <645733908@qq.com>
2017-05-24 11:29:38 +0800

    Update HelloGitHub.java

--- src/com/cognizant/test/HelloGitHub.java
+++ src/com/cognizant/test/HelloGitHub.java
@@ -7,14 +7,9 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 
 		System.out.println("Hello GitHub ");
-		
-		
-		System.out.println("Hello 22222 ");
-	
-		
-		System.out.println("Hello 233 ");
-		
 		System.out.println(new Date());
+	        System.out.println("更新到本地");
+		
 		
 	}
 
